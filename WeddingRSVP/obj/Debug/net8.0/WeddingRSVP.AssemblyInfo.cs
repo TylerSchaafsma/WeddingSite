102c42555beffa1eb6ab63ed1a139fcd75d299f4
Tyler Schaafsma <tschaaf7@gmail.com>
2026-04-01 00:31:48 -0400

    asdf

--- WeddingRSVP/obj/Debug/net8.0/WeddingRSVP.AssemblyInfo.cs
+++ WeddingRSVP/obj/Debug/net8.0/WeddingRSVP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeddingRSVP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88a1a4235788a01c3cdd80cdd37c1c1d88960c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeddingRSVP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeddingRSVP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
